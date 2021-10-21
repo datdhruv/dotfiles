f05ae209742b0c56465fc1f9f1ba4c5a5f860a5a
Dhruv <Dhruv>
2021-10-21 15:03:11 +0400

    fix forget-git-hist.sh

--- forget-git-hist.sh
+++ forget-git-hist.sh
@@ -3,4 +3,4 @@ git add -A  # Add all files and commit them
 git commit -m 'Add files'
 git branch -D main # Deletes the master branch
 git branch -m main # Rename the current branch to master
-git push -f origin master # Force push master branch to Git server
+git push -f origin main # Force push master branch to Git server
